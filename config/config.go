5b52712b88559f49900a1ac931a551a90ff50b30
Vadim <terranisu@gmail.com>
2018-12-08 23:03:07 +0100

    Add config package

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"os"
+
+  // process .env
+  "github.com/joho/godotenv"
+)
+
+func Load() map[string]string {
+  env := os.Getenv("GIFKA_ENV")
+  if env == "" {
+    env = "development"
+  }
+
+  var environment map[string]string
+  environment, err := godotenv.Read("config/.env." + env)
+
+  if (err != nil) {
+    panic(err)
+  }
+
+  return environment
+}
